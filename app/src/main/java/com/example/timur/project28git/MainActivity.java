73e8b5793b661af5c8a2bedb5cc32516a7f4f97b
ttashbaev <t.timurlan@gmail.com>
2018-04-19 21:05:43 +0600

    new branch

--- app/src/main/java/com/example/timur/project28git/MainActivity.java
+++ app/src/main/java/com/example/timur/project28git/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.timur.project28git;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
